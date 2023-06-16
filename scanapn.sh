c376a4815cb05e1142ec51cd68adfc7c7b15ac88
jeffhkgithub <99866341+jeffhkgithub@users.noreply.github.com>
2023-06-16 11:57:51 +0800

    Update scanapn.sh
    Update BU Name in front of it

--- scanapn.sh
+++ scanapn.sh
@@ -1,21 +1,21 @@
+#AllBU=TOBEADDED
+$AllBU=$(az account list --all -o tsv --query "[].name")
 
-#$AllBU=$(az account list --all -o tsv --query "[].name")
-AllBU="TOBEADDED"
 
 for BU in $AllBU
 do
-    echo $BUname
+    echo $BU
     az account set -n $BU
     echo "#############################################################################################"
     echo "Get the status of AppServie Plan"
     echo "#############################################################################################"
-    echo "AzureServiceType,AppServicePlan,Name,Status,Location,Pricing_Tier,isRedundant,isAutoScale,Instance_Count,MaxNumberWorker,ResourceGroup"
-    az appservice plan list --output tsv --query "[].{ AppServicePlan:id, Name:name, Status:status, Location:location, Pricing_Tier:sku.tier, isRedundant:zoneRedundant, IsAutoScale:elasticScaleEnabled, Instance_Count:numberOfWorkers, MaxNumberWorker:maximumNumberOfWorkers, ResourceGroup:resourceGroup}" | sed 's/\t/,/g' | sed 's/^/AppServiePlan,/g'
+    echo "BUName,AzureServiceType,AppServicePlan,Name,Status,Location,Pricing_Tier,isRedundant,isAutoScale,Instance_Count,MaxNumberWorker,ResourceGroup"
+    az appservice plan list --output tsv --query "[].{ AppServicePlan:id, Name:name, Status:status, Location:location, Pricing_Tier:sku.tier, isRedundant:zoneRedundant, IsAutoScale:elasticScaleEnabled, Instance_Count:numberOfWorkers, MaxNumberWorker:maximumNumberOfWorkers, ResourceGroup:resourceGroup}" | sed 's/\t/,/g' | sed 's/^/AppServiePlan,/g' | sed 's|^|'$BU,'|g'
     echo "#############################################################################################"
     echo "Get the status of Webapp & FunctionApp & LogicApp"
     echo "#############################################################################################"
-    echo "AzureServiceType,AppName,Name,Location,Type,Status,AppServicePlan_ID,isRedundant"
-    az webapp list --output tsv --query "[].{AppName:name, Location:location, Type:kind, Status:state, AppServicePlan:appServicePlanId, isRedundant:redundancyMode}" | sed 's/\t/,/g' | sed 's/^/WebApp,/g'
-    az functionapp list --output tsv --query "[].{AppName:name, Location:location, Type:kind, Status:state, AppServicePlan:appServicePlanId, isRedundant:redundancyMode}"| sed 's/,/_/g' | sed 's/\t/,/g' | sed 's/^/FunctionApp,/g'
-    az logicapp list --output tsv --query "[].{AppName:name, Location:location, Type:kind, Status:state, AppServicePlan:appServicePlanId, isRedundant:redundancyMode}" | sed 's/\t/,/g' | sed 's/^/LogicApp,/g'
+    echo "BUName,AzureServiceType,AppName,Location,Type,Status,AppServicePlan_ID,isRedundant"
+    az webapp list --output tsv --query "[].{AppName:name, Location:location, Type:kind, Status:state, AppServicePlan:appServicePlanId, isRedundant:redundancyMode}" | sed 's/,/_/g'| sed 's/\t/,/g' | sed 's/^/WebApp,/g' | sed 's|^|'$BU,'|g'
+    az functionapp list --output tsv --query "[].{AppName:name, Location:location, Type:kind, Status:state, AppServicePlan:appServicePlanId, isRedundant:redundancyMode}"| sed 's/,/_/g' | sed 's/\t/,/g' | sed 's/^/FunctionApp,/g' | sed 's|^|'$BU,'|g'
+    az logicapp list --output tsv --query "[].{AppName:name, Location:location, Type:kind, Status:state, AppServicePlan:appServicePlanId, isRedundant:redundancyMode}" | sed 's/,/_/g'| sed 's/\t/,/g' | sed 's/^/LogicApp,/g'| sed 's|^|'$BU,'|g'
 done
